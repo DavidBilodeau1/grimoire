ce16c5b91a3b67d3a1e4d0a2b63421c974caf16c
David Bilodeau <davidbilodeau1@hotmail.com>
2025-03-04 21:30:51 -0500

    Finished welcome page

--- app/Livewire/ListBookshelfBooks.php
+++ app/Livewire/ListBookshelfBooks.php
@@ -3,8 +3,10 @@
 namespace App\Livewire;
 
 use App\Models\Book;
+use App\Models\BookBookshelf;
 use Filament\Forms\Concerns\InteractsWithForms;
 use Filament\Forms\Contracts\HasForms;
+use Filament\Tables\Columns\ImageColumn;
 use Filament\Tables\Concerns\InteractsWithTable;
 use Filament\Tables\Contracts\HasTable;
 use Filament\Tables\Table;
@@ -30,13 +32,23 @@ class ListBookshelfBooks extends Component implements HasForms, HasTable
                     $q->where('bookshelf_id', $bookshelfId);
                     $q->orderBy('book_bookshelf.position', 'asc');
                 });
-            }))
+            })->join('book_bookshelf', 'book_bookshelf.book_id', 'books.id'))
             ->columns([
-                Tables\Columns\TextColumn::make('title'),
-                Tables\Columns\TextColumn::make('author'),
-                Tables\Columns\TextColumn::make('publisher'),
+                ImageColumn::make('cover_image')
+                    ->height('200px')
+                    ->extraAttributes([
+                        'style' => 'margin:-12px -40px 0px -16px',
+                    ])
+                    ->extraImgAttributes([
+                        'class' => 'object-cover h-fit rounded-t-xl w-full',
+                    ])
+                    ->alignJustify()
+                    ->url(fn (Book $record): string => route('books.show', ['book' => $record])),
+                Tables\Columns\TextColumn::make('title')->searchable()->sortable(),
+                Tables\Columns\TextColumn::make('author')->searchable()->sortable(),
+                Tables\Columns\TextColumn::make('publisher')->searchable()->sortable(),
             ])
-            ->reorderable('position');
+            ->reorderable('book_bookshelf.position');
     }
 
     public function render()
